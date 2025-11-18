40871f9a3c9092a39734b9bf1d760bb794bf65af
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-18 23:05:27 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -4,4 +4,8 @@ TypeCommands = {
   "console.log": function(c) { console.log(c) },
   "console.error": function(c) { console.error(c) },
   "console.warn": function(c) { console.warn(c) },
+  "let": function(n, z) { let Peremens[n] = z },
+  "var": function(n, z) { Peremens[n] = z },
+  "if": function(b, c) { if (b) { eval(RunTypeCode(c)) } },
+  "if else": function(b, c, e) { if (b) { eval(RunTypeCode(c)) } else { eval(RunTypeCode(e)) } }
 }
