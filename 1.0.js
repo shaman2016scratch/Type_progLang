6d74af203b04363322d69b80dffdae5bce695b78
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-18 23:10:16 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -7,5 +7,15 @@ TypeCommands = {
   "let": function(n, z) { let Peremens[n] = z },
   "var": function(n, z) { Peremens[n] = z },
   "if": function(b, c) { if (b) { eval(RunTypeCode(c)) } },
-  "if else": function(b, c, e) { if (b) { eval(RunTypeCode(c)) } else { eval(RunTypeCode(e)) } }
+  "if else": function(b, c, e) { if (b) { eval(RunTypeCode(c)) } else { eval(RunTypeCode(e)) } },
+  "eval": function(c) { eval(RunTypeCode(c)) },
+  "return": function(z) { return z },
+}
+TypeReporters = {
+  "text": function(t) { return t },
+  "==": function(a, b) { return a === b },
+  "<": function(a, b) { return a < b },
+  ">": function(a, b) { return a > b },
+  "eval": function(c) { eval(RunTypeCode(c)) },
+  "typeof": function(t) { return typeof t },
 }
