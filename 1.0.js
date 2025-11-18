c2b3da71b4aa85835b2f673cd425db090460fb4c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-18 23:00:37 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -1,2 +1,7 @@
 // Type code
 Peremens = {}
+TypeCommands = {
+  "console.log": function(c) { console.log(c) },
+  "console.error": function(c) { console.error(c) },
+  "console.warn": function(c) { console.warn(c) },
+}
