47c41b297614a86cc844f3ed14723da5e5177400
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-19 21:37:23 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -1,19 +1,19 @@
 // Type code
 Peremens = {}
 TypeCommands = {
-  "console.log": function(c) { console.log(c) },
-  "console.error": function(c) { console.error(c) },
-  "console.warn": function(c) { console.warn(c) },
-  "let": function(n, z) { let Peremens[n] = z },
-  "var": function(n, z) { Peremens[n] = z },
-  "if": function(b, c) { if (b) { eval(RunTypeCode(c)) } },
-  "if else": function(b, c, e) { if (b) { eval(RunTypeCode(c)) } else { eval(RunTypeCode(e)) } },
-  "eval": function(c) { eval(RunTypeCode(c)) },
-  "return": function(z) { return z },
-  "const": function(n, z) { const Peremens[n] = z },
-  "json": function(p, e, z) { Peremens[p] = eval(`Peremens[p].${e} = ${z}; Peremens[p]`) },
-  "for": function(e, c) { for(e) { RunTypeCode(c) } },
-  "while": function(i, c) { while (i) { RunTypeCode(c) } },
+  "console.log": function(c) { console.log(GetTypeReporter(c)) },
+  "console.error": function(c) { console.error(GetTypeReporter(c)) },
+  "console.warn": function(c) { console.warn(GetTypeReporter(c)) },
+  "let": function(n, z) { let Peremens[GetTypeReporter(n)] = GetTypeReporter(z) },
+  "var": function(n, z) { Peremens[GetTypeReporter(n)] = GetTypeReporter(z) },
+  "if": function(b, c) { if (b) { eval(RunTypeCode(GetTypeReporter(c))) } },
+  "if else": function(b, c, e) { if (b) { eval(RunTypeCode(GetTypeReporter(c))) } else { eval(RunTypeCode(GetTypeReporter(e))) } },
+  "eval": function(c) { eval(RunTypeCode(GetTypeReporter(c))) },
+  "return": function(z) { return GetTypeReporter(z) },
+  "const": function(n, z) { const Peremens[GetTypeReporter(n)] = GetTypeReporter(z) },
+  "json": function(p, e, z) { Peremens[GetTypeReporter(p)] = eval(`Peremens[${GetTypeReporter(p)}].${GetTypeReporter(e)} = ${GetTypeReporter(z)}`) },
+  "for": function(e, c) { for(GetTypeReporter(e)) { RunTypeCode(GetTypeReporter(c)) } },
+  "while": function(i, c) { while (GetTypeReporter(i)) { RunTypeCode(GetTypeReporter(c)) } },
 }
 TypeReporters = {
   "text": function(t) { return t },
@@ -27,11 +27,32 @@ TypeReporters = {
   "new number": function() { return 0 },
   "new array": function() { return [] },
   "new object": function() { return {} },
-  "json": function(p, e) { return eval(`return Peremens[p].${e}`) },
+  "json": function(p, e) { return eval(`Peremens[p].${e}`) },
   "true": function() { return true },
   "false": function() { return false },
   "not": function(b) { return !b },
   "<=": function(a, b) { return a <= b },
   ">=": function(a, b) { return a >= b },
   "and": function(a, b) { return a && b },
+}
+function RunTypeCode(c) {
+  TypeLang = {}
+  codeType = c.split("@;")
+  TypeSysFunc = {
+    "let a main()": function(c) { return c.split(";") },
+  }
+  for(i = 0; i < codeType.length; i++) {
+    TypeLang.command = codeType[i]
+    if (TypeLang.command === 'let a main(type)') {
+      TypeLang.main = {}; i++; TypeLang.main.code = TypeSysFunc['let a main()'](TypeLang.command)
+      for(TypeLang.main.i = 0; TypeLang.main.i < TypeLang.main.code.length; TypeLang.main.i++) {
+        i++
+        TypeLang.main.command = TypeLang.mian.code[TypeLang.main.i]
+        eval(`TypeCommands.${TypeLang.main.command}`)
+      }
+    }
+  }
+}
+function GetTypeReporter(r) {
+  return eval(`TypeReporters.${r}`)
 }
